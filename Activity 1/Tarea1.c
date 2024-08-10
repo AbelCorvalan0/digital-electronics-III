88685b96456d3a3170dc1b661dcef0e4433203b8
AbelCorvalan0 <abel.corvalan@mi.unc.edu.ar>
2024-08-10 14:42:55 -0300

    Add activity 1 comments

--- Activity 1/Tarea1.c
+++ Activity 1/Tarea1.c
@@ -2,15 +2,19 @@
 
 void main(){
 
+    // Define matrix.
     int matrix[]= { 3, 4, 1, 1 };
+    // Define accumulator variable.
     int accumulator= 0;
-
+    // Matrix length calculation.
     int size= sizeof(matrix)/sizeof(matrix[0]);
 
+    // Add whole matrix elements.
     for(int i=0; i<size; i++){
         accumulator+= matrix[i];
     }
     
+    // Print accumulator value.
     printf("The accumulated value is %d", accumulator);
 
     return;
